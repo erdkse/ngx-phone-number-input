ab395b7219a6b9dc2f4ee9c437506afc25e4b79b
Erdi Köse <erdikose8@gmail.com>
2018-10-05 00:19:04 +0300

    country selection

--- projects/phone-input/src/lib/phone-input.component.ts
+++ projects/phone-input/src/lib/phone-input.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, ViewChild, Renderer2, HostListener, ElementRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  Renderer2,
+  HostListener,
+  ElementRef
+} from '@angular/core';
 
 @Component({
   selector: 'ngx-phone-input',
@@ -6,28 +13,39 @@ import { Component, OnInit, ViewChild, Renderer2, HostListener, ElementRef } fro
   styleUrls: ['./phone-input.component.scss']
 })
 export class PhoneInputComponent implements OnInit {
-
-  @ViewChild('menu') menu;
+  public selectedCountry;
+  @ViewChild('menu')
+  menu;
   @HostListener('document:click', ['$event.target'])
   public onClick(targetElement) {
     const clickedInside = this.elementRef.nativeElement.contains(targetElement);
     if (!clickedInside && this.menu.nativeElement.classList.contains('show')) {
-        this.renderer.removeClass(this.menu.nativeElement, 'show');
-      }
+      this.renderer.removeClass(this.menu.nativeElement, 'show');
     }
+  }
 
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
-  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  pickCountry(country) {
+    this.selectedCountry = country;
+    this.closeMenu();
   }
 
   toggleMenu() {
     if (this.menu.nativeElement.classList.contains('show')) {
-      this.renderer.removeClass(this.menu.nativeElement, 'show');
+      this.closeMenu();
     } else {
-      this.renderer.addClass(this.menu.nativeElement, 'show');
+      this.openMenu();
     }
   }
 
+  closeMenu() {
+    this.renderer.removeClass(this.menu.nativeElement, 'show');
+  }
+
+  openMenu() {
+    this.renderer.addClass(this.menu.nativeElement, 'show');
+  }
 }
