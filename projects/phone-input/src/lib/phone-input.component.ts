7995133bb7f207f355d494f15289d5790954c8bb
Erdi Köse <me@erdkse.com>
2019-03-29 13:53:39 +0300

    pick country

--- projects/phone-input/src/lib/phone-input.component.ts
+++ projects/phone-input/src/lib/phone-input.component.ts
@@ -8,16 +8,15 @@ import {
   forwardRef,
   ChangeDetectionStrategy,
   OnChanges,
-  SimpleChanges
+  SimpleChanges,
+  Input
 } from '@angular/core';
 import {
   parsePhoneNumberFromString,
   AsYouType,
   getCountryCallingCode
 } from 'libphonenumber-js';
-import { COUNTRY_CODES } from './data';
 import { countries, countriesIso } from './phoneCodeCountries';
-// import examples from 'libphonenumber-js/examples.mobile.json';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
@@ -35,9 +34,17 @@ import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 })
 export class PhoneInputComponent
   implements OnInit, OnChanges, ControlValueAccessor {
-  public countryCodes: Array<string> = COUNTRY_CODES;
+  @Input() disabled = false;
+  @Input() defaultCountry;
+  public countries: Array<any> = countries;
   public selectedCountry;
   public phoneNumber = '';
+  public selectedCountryIndex = 0;
+  public tempSelectedCountryIndex = 0;
+  public itemHeight = 32;
+  public isCountrySelectionFocused = false;
+  public isMenuOpen = false;
+
   @ViewChild('menu')
   menu;
   @HostListener('document:click', ['$event.target'])
@@ -52,16 +59,23 @@ export class PhoneInputComponent
 
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.defaultCountry) {
+      const selectedCountry = this.countries.find(
+        c => c.iso2 === this.defaultCountry
+      );
+      this.pickCountry(selectedCountry ? selectedCountry : this.countries[0]);
+    }
+  }
 
   ngOnChanges(changes: SimpleChanges): void {}
-  writeValue(obj: any): void {}
-  registerOnChange(fn: any): void {}
-  registerOnTouched(fn: any): void {}
-  setDisabledState?(isDisabled: boolean): void {}
 
   pickCountry(country) {
     this.selectedCountry = country;
+    this.selectedCountryIndex = this.countries.findIndex(
+      c => c.iso2 === this.selectedCountry.iso2
+    );
+    this.tempSelectedCountryIndex = this.selectedCountryIndex;
     this.phoneNumberChanged(this.phoneNumber);
     this.closeMenu();
   }
@@ -74,16 +88,94 @@ export class PhoneInputComponent
     }
   }
 
+  onCountrySelectionButtonClicked() {
+    if (
+      !this.menu.nativeElement.classList.contains('show') &&
+      this.isCountrySelectionFocused
+    ) {
+      this.openMenu();
+    }
+  }
+
+  onFocused() {
+    this.openMenu();
+    this.isCountrySelectionFocused = true;
+  }
+
+  onBlured(event) {
+    if (
+      event.type === 'blur' &&
+      this.menu.nativeElement.contains(event.target)
+    ) {
+      this.closeMenu();
+      this.isCountrySelectionFocused = false;
+    }
+
+    if (
+      event.type === 'keydown' &&
+      (event.keyCode === 9 || event.keyCode === 27)
+    ) {
+      this.closeMenu();
+      this.isCountrySelectionFocused = false;
+    }
+  }
+
+  keyboardNav(e) {
+    const code = e.keyCode;
+    console.log(code);
+    if (this.menu.nativeElement.classList.contains('show')) {
+      if (code === 40) {
+        this.tempSelectedCountryIndex =
+          this.tempSelectedCountryIndex >= this.countries.length - 1
+            ? this.countries.length - 1
+            : this.tempSelectedCountryIndex + 1;
+      }
+      if (code === 38) {
+        this.tempSelectedCountryIndex =
+          this.tempSelectedCountryIndex < 1
+            ? 0
+            : this.tempSelectedCountryIndex - 1;
+      }
+
+      if (code === 13) {
+        this.pickCountry(this.countries[this.tempSelectedCountryIndex]);
+        e.preventDefault();
+      }
+
+      if (code === 27 || code === 9) {
+        this.onBlured(e);
+      }
+
+      this.renderer.setProperty(
+        this.menu.nativeElement,
+        'scrollTop',
+        this.tempSelectedCountryIndex * this.itemHeight - this.itemHeight * 3
+      );
+    } else {
+      if (this.isCountrySelectionFocused && code === 13) {
+        this.openMenu();
+      }
+    }
+  }
+
   closeMenu() {
     this.renderer.removeClass(this.menu.nativeElement, 'show');
   }
 
   openMenu() {
     this.renderer.addClass(this.menu.nativeElement, 'show');
+    this.renderer.setProperty(
+      this.menu.nativeElement,
+      'scrollTop',
+      this.tempSelectedCountryIndex * this.itemHeight - this.itemHeight * 3
+    );
   }
 
   isCountrySelected(country): boolean {
-    return country === this.selectedCountry;
+    if (!this.selectedCountry) {
+      return false;
+    }
+    return country.iso2 === this.selectedCountry.iso2;
   }
 
   getCountryCallingCode(country) {
@@ -92,8 +184,14 @@ export class PhoneInputComponent
 
   phoneNumberChanged(number: string) {
     if (this.selectedCountry) {
-      const asYouType = new AsYouType(this.selectedCountry.toUpperCase());
+      const asYouType = new AsYouType(this.selectedCountry.iso2.toUpperCase());
       this.phoneNumber = asYouType.input(number);
     }
   }
+
+  writeValue(obj: any): void {}
+  registerOnChange(fn: any): void {}
+  registerOnTouched(fn: any): void {}
+  setDisabledState?(isDisabled: boolean): void {}
+  u;
 }
--- projects/phone-input/src/lib/phone-input.module.ts
+++ projects/phone-input/src/lib/phone-input.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PhoneInputComponent } from './phone-input.component';
 import { CommonModule } from '@angular/common';
-import { CountrySelectorComponent } from './country-selector/country-selector.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [PhoneInputComponent, CountrySelectorComponent],
+  declarations: [PhoneInputComponent],
   exports: [PhoneInputComponent]
 })
 export class PhoneInputModule {}
